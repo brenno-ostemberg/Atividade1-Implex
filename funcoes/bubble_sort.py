6470c510b3e60ff762a9c06540b56dd3ffd2bd3b
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2024-09-11 19:37:23 -0400

    feat: criando uma classe no arquivo bubble_sort.py para facilitar a importacao no handler

--- funcoes/bubble_sort.py
+++ funcoes/bubble_sort.py
@@ -1,7 +1,10 @@
-def bubble_sort(vetor):
-    n = len(vetor)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if vetor[j] > vetor[j + 1]:
-                vetor[j], vetor[j + 1] = vetor[j + 1], vetor[j] 
-    return vetor
+class BubbleSort:
+
+    @staticmethod
+    def bubble_sort(vetor):
+        n = len(vetor)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if vetor[j] > vetor[j + 1]:
+                    vetor[j], vetor[j + 1] = vetor[j + 1], vetor[j] 
+        return vetor
