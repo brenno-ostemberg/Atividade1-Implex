b8cbb33fb4725103a2fad5aa15e0d4f4d5e4b302
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2024-09-11 19:45:26 -0400

    feat: importando todas as classes dos arquivos de ordenação

--- handler.py
+++ handler.py
@@ -1,8 +1,12 @@
 import random
 import time 
 import timeit
-from funcoes.bubble_sort import bubble_sort 
-from funcoes.insertion_sort import insertion_sort
+from funcoes.bubble_sort import BubbleSort
+from funcoes.insertion_sort import InsertionSort
+from funcoes.quick_sort import QuickSort
+from funcoes.counting_sort import CountingSort
+from funcoes.heap_sort import HeapSort
+from funcoes.merge_sort import MergeSort
 
 def vetor_aleatorio(inc, fim, stp):
     vetores = {}
@@ -45,7 +49,7 @@ for n, vetor in vetores_gerados.items(0):
 print("[[RANDOM]]")
 inicio = timeit.default_timer()
 for n, vetor in vetores_aleatorios.items():
-    bubble_sort(vetor)
+    BubbleSort.bubble_sort(vetor)
     break
 fim = timeit.default_timer()
 print ('duracao bubble sort: %f' % (fim - inicio))
@@ -54,7 +58,7 @@ print ('duracao bubble sort: %f' % (fim - inicio))
 #---------------------------------Insertion--------------------
 inicio = timeit.default_timer()
 for n, vetor in vetores_aleatorios.items():
-    insertion_sort(vetor)
+    InsertionSort.insertion_sort(vetor)
     break
 fim = timeit.default_timer()
 print ('duracao insertion sort: %f' % (fim - inicio))
