bf58ab33f24404eb1800bf49e4d13fe5cce8afb2
juliacamposn <neryjuliacampos@gmail.com>
2024-09-11 16:34:15 -0400

    wip

--- handler.py
+++ handler.py
@@ -1,4 +1,7 @@
 import random
+import time 
+import timeit
+from funcoes.bubble_sort import bubble_sort 
 
 def gerar_vetores(inc, fim, stp):
     vetores = {}
@@ -16,9 +19,17 @@ stp = 1000
 vetores_gerados = gerar_vetores(inc, fim, stp)
 
 ''' Exibindo um exemplo de vetor gerado
-for n, vetor in vetores_gerados.items():
+for n, vetor in vetores_gerados.items(0):
     print(f"Vetor com {n} elementos: {vetor[:5]}...")  
-
 '''
+
 print("[[RANDOM]]")
+inicio = timeit.default_timer()
+
+for n, vetor in vetores_gerados.items():
+    bubble_sort(vetor)
+    break
+
+fim = timeit.default_timer()
+print ('duracao: %f' % (fim - inicio))
 
