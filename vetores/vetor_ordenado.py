c659167113afc17313ecdbcdf98604855fad4547
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2024-09-13 15:49:34 -0400

    fix: arrumando função que cria os vetores ordenados

--- vetores/vetor_ordenado.py
+++ vetores/vetor_ordenado.py
@@ -2,7 +2,7 @@ def vetor_ordenado(inc, fim, stp):
     vetores = {}
     
     for n in range(inc, fim + 1, stp):
-        vetor = [i for i in range(0, n, stp)]
+        vetor = [i for i in range(1, n + 1)]
         vetores[n] = vetor
         
     return vetores
