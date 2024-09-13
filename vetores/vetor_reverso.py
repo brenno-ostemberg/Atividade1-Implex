5a10f0294d23b38bb1de4d5c2efd24712e202e65
brenno-ostemberg <brenno.ostemberg.dev@gmail.com>
2024-09-13 15:49:49 -0400

    fix: arrumando função que cria os vetores reversos

--- vetores/vetor_reverso.py
+++ vetores/vetor_reverso.py
@@ -2,8 +2,7 @@ def vetor_reverso(inc, fim, stp):
     vetores = {}
     
     for n in range(inc, fim + 1, stp):
-        vetor = [i for i in range(n, 0, -stp)]
+        vetor = [i for i in range(n, 0, -1)]
         vetores[n] = vetor
         
     return vetores
-
